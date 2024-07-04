6f5d220e3ecaa0218543aec5b060a685981f5c02
neel <neelramnani44444@gmail.com>
2024-07-05 01:08:10 +0500

    img work

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -1,24 +1,32 @@
-
-
 class Api::ImagesController < ApplicationController
- 
-  def create
-    @image = Image.new(image_params)
-
-    if @image.save
-      render json: @image, status: :created
-    else
-      render json: { errors: @image.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
   def index
     user_id = params[:user_id]
-    images = Image.where(user_id: user_id)
-    render json: images, status: :ok
+    images = Image.where(user_id: user_id).with_attached_image_file
+    render json: images.map { |image| image_with_attachment(image) }
   end
+
+  def create
+    image = Image.new(image_params)
+    image.user_id = params[:image][:user_id]
+
+    if image.save
+      render json: image_with_attachment(image), status: :created
+    else
+      render json: { errors: image.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def image_params
-    params.require(:image).permit(:image_file, :prompt, :user_id)
+    params.require(:image).permit(:prompt, :image_file)
+  end
+
+  def image_with_attachment(image)
+    {
+      id: image.id,
+      prompt: image.prompt,
+      image_file_url: image.image_file.attached? ? url_for(image.image_file) : nil
+    }
   end
 end
